40dc686b4a2e806420546dfc585f1234440cc5bf
yeh8211TK <U401260048@gmail.com>
2020-10-21 23:04:20 +0800

    Updates

---
+++ precache-manifest.5226e8b24b85e70bc3a4a3583170ca66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40daffdee50576dc10b0e72ebb046e0b",
+    "url": "/exhibition-app/index.html"
+  },
+  {
+    "revision": "98c675617ab4aeff5818",
+    "url": "/exhibition-app/static/css/main.7bca7b89.chunk.css"
+  },
+  {
+    "revision": "7a2a53c49e8a22169c48",
+    "url": "/exhibition-app/static/js/2.f7496a61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/exhibition-app/static/js/2.f7496a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c675617ab4aeff5818",
+    "url": "/exhibition-app/static/js/main.a1c42d78.chunk.js"
+  },
+  {
+    "revision": "62fa59c1147ce2a49d4e",
+    "url": "/exhibition-app/static/js/runtime-main.c7889257.js"
+  }
+]);
